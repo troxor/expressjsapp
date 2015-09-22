989875d7abef2f4d22511a867078689773c0e39b
Troy C <troxor@users.noreply.github.com>
2015-09-22 12:50:21 -0500

    update express to 4.10.2

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('index\n');
+	res.send('index<hr/>');
 });
 
+app.get('/version', function(req, res) {
+	res.type('text/plain');
+	res.send('version');
+});
 
 var server = app.listen(3000, function(){
 	console.log('Listening on port %d', server.address().port);
